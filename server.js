e8cbf35753a69160e80a39f4264b7ffc6ea563e6
Kundan Kumar <kundan.kumar@ekaaya.in>
2025-06-13 11:32:15 +0530

    latest code

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 require('./db'); // DB connection
 const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
+
 // Routes
 app.use('/api', require('./routes/books.routes'));   // /api/books
 app.use('/api', require('./routes/person.routes'));  // all person routes with custom paths
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('🚀 Server running on http://localhost:3000');
 });
